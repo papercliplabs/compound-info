f2faf97085fc3e9337ed033d79f5405c857a3828
Cole Perkins <cole@Coles-MacBook-Pro-2.local>
2021-05-24 21:49:53 -0700

    removed double quotes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,33 +1,33 @@
-import React from "react";
-import styled from "styled-components";
-import { Typography } from "theme";
-import { formatNumber } from "utils";
+import React from 'react'
+import styled from 'styled-components'
+import { Typography } from 'theme'
+import { formatNumber } from 'utils'
 
 const Card = styled.div`
 	display: flex;
 	border: solid
-		${({ theme }) => theme.border.thickness + " " + theme.color.border1};
+		${({ theme }) => theme.border.thickness + ' ' + theme.color.border1};
 	border-radius: ${({ theme }) => theme.border.radius};
 	background-color: ${({ theme }) => theme.color.bg1};
-	width: ${({ width }) => width ?? "100%"};
+	width: ${({ width }) => width ?? '100%'};
 	padding: ${({ padding, theme }) => padding ?? theme.spacing.roomy};
 	margin: 0;
 	box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1),
 		inset 0px 1px 6px rgba(255, 255, 255, 0.05);
 	row-gap: 8px;
-`;
+`
 
 const StyledStatCard = styled(Card)`
 	flex-direction: column;
-`;
+`
 
 export function StatCard({ title, value, unit }) {
-	const formattedValue = formatNumber(value, unit);
+	const formattedValue = formatNumber(value, unit)
 	return (
 		<StyledStatCard>
 			<Typography.header>{title}</Typography.header>
 			<Typography.displayL>{formattedValue}</Typography.displayL>
 		</StyledStatCard>
-	);
+	)
 }
-export default Card;
+export default Card
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 import styled, {
 	css,
 	createGlobalStyle,
 	ThemeProvider,
-} from "styled-components";
+} from 'styled-components'
 
 // Media queries
 const mediaQuerySizes = {
@@ -11,48 +11,48 @@ const mediaQuerySizes = {
 	small: 700,
 	medium: 960,
 	large: 1280,
-};
+}
 
 const mediaQueries = Object.keys(mediaQuerySizes).reduce((acc, key) => {
 	acc[key] = (...args) => css`
 		@media (max-width: ${mediaQuerySizes[key]}px) {
 			${css(...args)};
 		}
-	`;
-	return acc;
-}, {});
+	`
+	return acc
+}, {})
 
 // General theme
 const theme = (darkMode) => ({
 	color: {
-		bg0: darkMode ? "#1B191D" : "#FFFFFF",
-		bg1: darkMode ? "#27272B" : "#FFFFFF",
-		bg2: darkMode ? "#333338" : "#FFFFFF",
+		bg0: darkMode ? '#1B191D' : '#FFFFFF',
+		bg1: darkMode ? '#27272B' : '#FFFFFF',
+		bg2: darkMode ? '#333338' : '#FFFFFF',
 
-		primary1: darkMode ? "#FFFFFF" : "#212B36",
-		secondary1: darkMode ? "#AAAAAA" : "#586069",
+		primary1: darkMode ? '#FFFFFF' : '#212B36',
+		secondary1: darkMode ? '#AAAAAA' : '#586069',
 
-		active1: darkMode ? "#000000" : "#0366D6",
+		active1: darkMode ? '#000000' : '#0366D6',
 
-		border1: darkMode ? "#272B31" : "#DEDFE1",
+		border1: darkMode ? '#272B31' : '#DEDFE1',
 
-		linkInternal: darkMode ? "#0095D5" : "#0095D5",
-		linkExternal: darkMode ? "#808994" : "#586069",
+		linkInternal: darkMode ? '#0095D5' : '#0095D5',
+		linkExternal: darkMode ? '#808994' : '#586069',
 	},
 
 	border: {
-		thickness: "1px",
-		radius: "16px",
+		thickness: '1px',
+		radius: '16px',
 	},
 
 	mediaWidth: mediaQueries,
 
 	spacing: {
-		default: "8px",
-		card: "16px",
-		roomy: "24px",
+		default: '8px',
+		card: '16px',
+		roomy: '24px',
 	},
-});
+})
 
 // Typography theme
 const StyledText = styled.div`
@@ -60,7 +60,7 @@ const StyledText = styled.div`
 	font-size: ${({ fontSize }) => fontSize}px;
 	font-weight: ${({ fontWeight }) => fontWeight};
 	line-height: ${({ lineHeight }) => lineHeight}px;
-`;
+`
 
 export const Typography = {
 	displayXL(props) {
@@ -68,94 +68,94 @@ export const Typography = {
 			<StyledText
 				fontSize={42}
 				fontWeight={600}
-				color={"primary1"}
+				color={'primary1'}
 				lineHeight={44}
 				{...props}
 			/>
-		);
+		)
 	},
 	displayL(props) {
 		return (
 			<StyledText
 				fontSize={28}
 				fontWeight={500}
-				color={"primary1"}
+				color={'primary1'}
 				lineHeight={32}
 				{...props}
 			/>
-		);
+		)
 	},
 	displayM(props) {
 		return (
 			<StyledText
 				fontSize={26}
 				fontWeight={600}
-				color={"primary1"}
+				color={'primary1'}
 				lineHeight={32}
 				{...props}
 			/>
-		);
+		)
 	},
 	displayS(props) {
 		return (
 			<StyledText
 				fontSize={20}
 				fontWeight={600}
-				color={"primary1"}
+				color={'primary1'}
 				lineHeight={28}
 				{...props}
 			/>
-		);
+		)
 	},
 	header(props) {
 		return (
 			<StyledText
 				fontSize={16}
 				fontWeight={500}
-				color={"primary1"}
+				color={'primary1'}
 				lineHeight={24}
 				{...props}
 			/>
-		);
+		)
 	},
 	subheader(props) {
 		return (
 			<StyledText
 				fontSize={14}
 				fontWeight={400}
-				color={"secondary1"}
+				color={'secondary1'}
 				lineHeight={28}
 				{...props}
 			/>
-		);
+		)
 	},
 	body(props) {
 		return (
 			<StyledText
 				fontSize={16}
 				fontWeight={400}
-				color={"primary1"}
+				color={'primary1'}
 				lineHeight={28}
 				{...props}
 			/>
-		);
+		)
 	},
 	caption(props) {
 		return (
 			<StyledText
 				fontSize={14}
 				fontWeight={500}
-				color={"primary1"}
+				color={'primary1'}
 				lineHeight={28}
 				{...props}
 			/>
-		);
+		)
 	},
-};
+}
 
 // Everything that is child of theme, gets theme as its props, so it can be used everywhere
 export default function Theme({ darkMode, children }) {
-	return <ThemeProvider theme={theme(darkMode)}>{children}</ThemeProvider>;
+	return <ThemeProvider theme={theme(darkMode)}>{children}</ThemeProvider>
 }
 
 // Fonts are imported in index.html
@@ -191,4 +191,4 @@ export const GlobalStyle = createGlobalStyle`
 	a {
 		color: ${({ theme }) => theme.color.linkInternal}
 	}
-`;
+`
