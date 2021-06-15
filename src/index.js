7050d5617e776fa715d84454f49fdaec97cd6db7
Spencer <hello@spencerperkins.me>
2021-06-15 00:32:48 -0700

    Added google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import App from 'app';
 import Theme, { GlobalStyle } from 'theme';
 import GlobalStoreProvider from 'store';
+import ReactGA from 'react-ga';
+import { withRouter } from 'react-router-dom';
+
+const trackingId = 'UA-199555486-1';
+
+// Google Analytics
+ReactGA.initialize(trackingId);
+
+const RouteChangeTracker = ({ history }) => {
+	history.listen((location, action) => {
+		ReactGA.set({ page: location.pathname });
+		ReactGA.pageview(location.pathname);
+	});
+	return <div></div>;
+};
+
+ReactGA.exception({
+	description: 'An error ocurred',
+	fatal: true,
+});
 
 ReactDOM.render(
 	<StrictMode>
@@ -15,3 +35,5 @@ ReactDOM.render(
 	</StrictMode>,
 	document.getElementById('root')
 );
+
+export default withRouter(RouteChangeTracker);
